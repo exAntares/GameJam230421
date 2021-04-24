3563d6ccaa2e36e292913af2d51368b5598bb4d0
Luis Rojas <luis.andres81@gmail.com>
2021-04-24 20:16:02 +0200

    Improved dragging experience

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IDragHandler {
-    public void OnDrag(PointerEventData eventData) {
-        if (eventData.pointerCurrentRaycast.isValid) {
-            var oldZ = gameObject.transform.position.z;
-            var worldPosition = eventData.pointerCurrentRaycast.worldPosition;
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+    public void OnDrag(PointerEventData eventData) => UpdatePosition(eventData);
 
+    public void OnBeginDrag(PointerEventData eventData) => UpdatePosition(eventData);
+
+    public void OnEndDrag(PointerEventData eventData) => UpdatePosition(eventData);
+
+    private void UpdatePosition(PointerEventData eventData) {
+        var myGameObject = gameObject;
+        var position = myGameObject.transform.position;
+        Vector3 worldPosition;
+        var oldZ = position.z;
+        if (eventData.pointerPressRaycast.isValid && eventData.pointerCurrentRaycast.worldPosition != Vector3.zero) {
+            worldPosition = eventData.pointerCurrentRaycast.worldPosition;
             worldPosition.z = oldZ;
-            gameObject.transform.position = worldPosition;
         }
+        else {
+            // this works because we use an orthographic camera
+            var screenToWorldPoint = eventData.pressEventCamera.ScreenToWorldPoint(eventData.position);
+            worldPosition = screenToWorldPoint;
+        }
+        
+        worldPosition.z = oldZ;
+        myGameObject.transform.position = worldPosition;
     }
 }
